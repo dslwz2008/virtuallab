5f8e4fa3becd8fdb0f922fe9f5f292e0d8b4e57e
dslwz2008 <dslwz2008@gmail.com>
2014-01-02 17:15:22 +0800

    modify

--- Assets/DemoVikings/Scripts/ListenNetwork.cs
+++ Assets/DemoVikings/Scripts/ListenNetwork.cs
@@ -3,17 +3,13 @@ using System.Collections;
 
 public class ListenNetwork : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-	
 	void OnEnable(){
-		
+		StartCoroutine("NetworkUtils.ListenAndHandle");
 	}
+	
+	void OnDisable(){
+		StopAllCoroutines();
+	}
+	
+	
 }
--- Assets/DemoVikings/Scripts/NetworkUtils.cs
+++ Assets/DemoVikings/Scripts/NetworkUtils.cs
@@ -5,8 +5,12 @@ using System.Collections.Generic;
 public class NetworkUtils {
 	
 	public static string serverUrl = "http://localhost:8000/images/";
+	public static int requestIntervel = 0.5;
+	private static string paraName = "";
+	private static int startNumber = 0, endNumber = 0;
 	
 	public static IEnumerator PostParameters(List<string> paras){
+		paraName = NetworkUtils.ParameterToString(paras);
 		WWWForm form = new WWWForm();
 		form.AddField("ambient", paras[0]);
 		form.AddField("cdom", paras[1]);
@@ -15,13 +19,63 @@ public class NetworkUtils {
 		form.AddField("fresnel_coeff", paras[4]);
 		form.AddField("influx", paras[5]);
 		form.AddField("wave_scale", paras[6]);
-		WWW postform = new WWW(serverUrl+"post_parameter", form);
+		WWW postform = new WWW(serverUrl + "post_parameter", form);
 		yield return postform;
 		if(!string.IsNullOrEmpty(postform.error)){
 			Debug.Log(postform.error);
 		}else{
 			Debug.Log(postform.text);
 		}
-		
+	}
+	
+	public static IEnumerator ListenAndHandle(){
+		bool needStop = true;
+		while(needStop){
+			WWWForm form = new WWWForm();
+			form.AddField("paraname", paraName);
+			WWW postform = new WWW(serverUrl + "get_status", form);
+			yield return postform;
+			if(!string.IsNullOrEmpty(postform.error)){
+				Debug.Log(postform.error);
+			}else{
+				Debug.Log(postform.text);
+				//examples:
+				//status:0;number:0
+				//status:1;number:15
+				//status:2;number:200
+				string[] pairs = postform.text.Split(";");
+				if(pairs.Length == 1){
+					Debug.Log("出错了！");
+					return null;
+				}
+				string status = pairs[0].Split(":")[1];
+				endNumber = int.Parse(pairs[1].Split(":")[1]);
+				switch(status){
+				case "0"://还没有回应，继续请求
+					yield return WaitForSeconds(requestIntervel);
+					break;
+				case "1"://没有回应完成，先取部分图片，继续请求
+					for(int index = startNumber; index < endNumber; index++){
+						WWW getimage = new WWW();
+					}
+					break;
+				case "2"://回应结束了，把剩下的图片全部取出，停止请求
+					
+					needStop = true;
+					break;
+				default://有错误
+					needStop = true;
+					break;
+				}
+			}
+		}
+	}
+	
+	public static string ParameterToString(List<string> paras){
+		string result = paras[0];
+		for(int i = 1; i < paras.Count; i++){
+			result += ("_"+paras[i]);
+		}
+		return result;
 	}
 }
